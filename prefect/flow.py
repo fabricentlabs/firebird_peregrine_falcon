a0047750556917bf317480f7da81f22affae00e4
Luan Fillipe <luanft9@gmail.com>
2025-11-21 15:29:18 -0300

    feat: Add prefect folder with flow.py to git prefect integration

--- prefect/flow.py
+++ prefect/flow.py
@@ -4,9 +4,9 @@ import subprocess
 import os
 
 @task
-def run_ps1_from_github():
+def run_ps1_from_github(dataabse: str, outDir: str, table: str) -> None:
     # Carrega o bloco GitHub criado no Prefect UI/CLI
-    repo = GitHubRepository.load("my-rust-repo")
+    repo = GitHubRepository.load("firebird_peregrine_falcon")
 
     # Clona / baixa o repositório para um diretório temporário
     repo_dir = repo.get_directory()
@@ -14,7 +14,6 @@ def run_ps1_from_github():
     # Caminho do script dentro do repositório clonado
     ps1_path = os.path.join(
         repo_dir,
-        "firebird_peregrine_falcon",
         "run_agile_log_obrigacao.ps1"
     )
 
@@ -36,9 +35,15 @@ def run_ps1_from_github():
 
 
 @flow(name="Get data from firebird", log_prints=True)
-def load_data_from_firebird():
-    return run_ps1_from_github()
+def load_data_from_firebird(database: str, outDir: str, tables: list[str]) -> None:
+    
+    for table in tables:
+        return run_ps1_from_github(database, outDir, table)
 
 
 if __name__ == "__main__":
-    load_data_from_firebird()
+    load_data_from_firebird(
+        database='C:\\consumer\\CONSUMER.FDB',
+        outDir='C:\\PARA\\1Projects\\Fabricent_Legnet\\data',
+        tables=['ITENSPEDIDO', 'PEDIDOS', 'PRODUTODETALHE']
+    )
